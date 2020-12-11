b054ec8a91b1bdfc38bafe1af2b51e1b82ab505a
crispthoalex <alex877@gmail.com>
2020-12-11 05:02:18 +0000

    Method to determine if all the boxes can be opened was added

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# Method that determines if all the boxes can be opened
+
+def uniquekeys(boxes, allkeys):
+    res = []
+    for e in allkeys:
+        res += boxes[e]
+    return res
+
+def canUnlockAll(boxes):
+    pos = 0
+    allkeys = list(set(boxes[0]) | {0})
+    adding = True
+
+    while adding:
+        adding = False
+        for j in uniquekeys(boxes, allkeys[pos:]):
+            if j not in allkeys:
+                allkeys.append(j)
+                pos += 1
+                adding = True
+
+    # There is one key for one box
+    return len(allkeys) == len(boxes)
