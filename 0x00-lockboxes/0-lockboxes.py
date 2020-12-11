6cbce2a69b355f8ce12320df6caa1efdba27555e
crispthoalex <alex877@gmail.com>
2020-12-11 06:51:01 +0000

    Guide style was fixed and clean comments

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,12 +4,7 @@
 
 def uniquekeys(boxes, allkeys):
     res = []
-    # Print the length of allkeys and boxes
-    # print("\nLenght of allkeys[pos:]= {}\nAnd allkeys = {}".format(len(allkeys), allkeys))
-    # print("Boxes Length = ", len(boxes))
     for k, e in enumerate(allkeys):
-        # print("\nk = {}, e = {}".format(k, e))
-        # print("\tValue in k = ", boxes[k])
         if e < len(boxes):
             res += boxes[e]
     return res
@@ -19,7 +14,7 @@ def canUnlockAll(boxes):
     pos = 0
     allkeys = list(set(boxes[0]) | {0})
     adding = True
-    # print("\n\nPrint allkeys ", allkeys)
+
     while adding:
         adding = False
         for j in uniquekeys(boxes, allkeys[pos:]):
@@ -27,6 +22,5 @@ def canUnlockAll(boxes):
                 allkeys.append(j)
                 pos += 1
                 adding = True
-    # There is one key for each box or more keys that boxes
-    # print([i for i in sorted(allkeys)])
+
     return len(allkeys) >= len(boxes)
