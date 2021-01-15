72bd6dffb5579c778ff085075138c4de572a31e8
crispthoalex <alex877@gmail.com>
2021-01-15 04:51:28 +0000

    Firs sum was added

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -8,5 +8,10 @@
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-	
+	int col = 0, row = 0;
+	for (; col < 3 ; col++)
+	{
+		for (; row < 3; row++)
+			grid1[col][row] = grid1[col][row] + grid2[col][row];
+	}
 }
