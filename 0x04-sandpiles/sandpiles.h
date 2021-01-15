829e9719ee7a1197dcf5dbdd6a745282b09a2dde
crispthoalex <alex877@gmail.com>
2021-01-15 07:22:45 +0000

    Check stability and print grid function were added

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -2,11 +2,11 @@
 #define _SANDPILES_H_
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
-
-static void print_grid_sum(int grid1[3][3], int grid2[3][3]);
-static void print_grid(int grid[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-
+bool isStable(int grid1[3][3]);
+void toppling(int grid1[3][3]);
+void other_print_grid(int grid[3][3]);
 
 #endif /* _SANDPILES_ */
