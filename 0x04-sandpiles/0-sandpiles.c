02dcc85e61f779af0d50f9e0a6e2c9b91a3fb264
crispthoalex <alex877@gmail.com>
2021-01-15 04:55:54 +0000

    Betty style was fixed

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -1,3 +1,5 @@
+#include <sandpiles.h>
+#include <stdio.h>
 /**
  * sandpiles_sum - computes the sum of two sandpiles
  *
@@ -5,10 +7,10 @@
  * @grid2: Grid 3x3 to add
  * Return: void
  */
-
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
 	int col = 0, row = 0;
+
 	for (; col < 3 ; col++)
 	{
 		for (; row < 3; row++)
