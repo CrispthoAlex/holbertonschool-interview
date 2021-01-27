a78f89c8cd88131026052dabe19d537331ca12c2
CrispthoAlex <alex877@gmail.com>
2021-01-27 04:57:16 -0500

    Guide style was fixed

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,10 +3,10 @@ import sys
 
 filesize = 0
 countLine = 0
-# Status code Counters
-c200 = c300 = c301 = c400 = c401 = c403 = c404 = c405 = c500 = 0
 # Dictionary to save status code counters
-codeKey = { 200:0, 300:0, 301:0, 400:0, 401:0, 403:0, 404:0, 405:0, 500:0 }
+codeKey = {
+    200: 0, 300: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0
+    }
 
 for line in sys.stdin:
     line = line.split()
