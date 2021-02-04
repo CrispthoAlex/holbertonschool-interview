2e104f9361b3c504e32a27a82175b1a6180d970d
crispthoalex <alex877@gmail.com>
2021-02-04 23:24:29 +0000

    Function to check linked list cycle was added

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -1,6 +1,23 @@
 #include "lists.h"
 
+/**
+ * check_cycle - check if there are cycle
+ * @list: head of list
+ *
+ * Return: 0 no cycle, 1 is cycle
+ */
 int check_cycle(listint_t *list)
 {
+	listint_t *current, *temp;
 
+	current = temp = list;
+
+	while (current && temp && temp->next)
+	{
+		current = current->next;
+		temp = temp->next->next;
+		if (current == temp)
+			return (1);
+	}
+	return (0);
 }
