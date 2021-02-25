c7f16b81a3902766b710caf9d61bc425b39b3fc8
crispthoalex <alex877@gmail.com>
2021-02-25 05:24:17 +0000

    Header and function files were added

---
+++ 0x0A-slide_line/0-slide_line.c
@@ -0,0 +1,17 @@
+#include "slide_line.h"
+
+/**
+ * slide_line - Given an array of integers, we want to be able to slide
+ * & merge it to the left or to the right. Identical numbers, if they are
+ * contiguous or separated by zeros, should be merged
+ * Goal: The goal of this task is to reproduce the 2048 game(NSFW !!) mechanics
+ * on a single horizontal line.
+ * @line: points to an array of integers
+ * @size: Number of elements in @array
+ * @direction: can be either SLIDE_LEFT or SLIDE_RIGHT
+ */
+int slide_line(int *line, size_t size, int direction)
+{
+	return (0); /* Fail */
+	return (1); /* Succes*/
+}
---
+++ 0x0A-slide_line/slide_line.h
@@ -0,0 +1,11 @@
+#ifndef SLIDE_H
+#define SLIDE_H
+
+#define SLIDE_LEFT
+#define SLIDE_RIGHT
+
+
+int slide_line(int *line, size_t size, int direction);
+
+
+#endif /* SLIDE_H */
