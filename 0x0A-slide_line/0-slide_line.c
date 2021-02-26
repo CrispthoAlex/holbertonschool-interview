be044673b63600e0e5eb6d42719b7ebce7d3bcb0
crispthoalex <alex877@gmail.com>
2021-02-26 04:55:18 +0000

    function that slides and merges an array of integers like 2048 game was added

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -1,30 +1,13 @@
 #include "slide_line.h"
 
-/**
- * slide_line - Given an array of integers, we want to be able to slide
- * & merge it to the left or to the right. Identical numbers, if they are
- * contiguous or separated by zeros, should be merged
- * Goal: The goal of this task is to reproduce the 2048 game(NSFW !!) mechanics
- * on a single horizontal line.
- * @line: points to an array of integers
- * @size: Number of elements in array
- * @direction: can be either SLIDE_LEFT or SLIDE_RIGHT
- * Return: 1 upon success, or 0 upon failure
- */
-int slide_line(int *line, size_t size, int direction)
-{
-	if (direction == SLIDE_LEFT) {moveLeft(line, size);}
-	if (direction == SLIDE_RIGHt) {moveRight(line, size);}
-	return (0); /* Fail */
-}
 
 /**
- * moveRight - Slides and merges an array of integers to the right
+ * move_right - Slides and merges an array of integers to the right
  * @line: Pointer to an array of integers
  * @size: Number of elements in array
  * Return: 1 is success
  */
-int moveRight(int *line, size_t size)
+void move_right(int *line, size_t size)
 {
 	int i, j;
 
@@ -47,16 +30,15 @@ int moveRight(int *line, size_t size)
 			}
 		}
 	}
-	return (1);
 }
 
 /**
- * moveLeft - Slides and merges an array of integers to the left
+ * move_left - Slides and merges an array of integers to the left
  * @line: Pointer to an array of integers
  * @size: Number of elements in array
  * Return: 1 is success
  */
-int moveLeft(int *line, size_t size)
+void move_left(int *line, size_t size)
 {
 	int i, j;
 
@@ -79,5 +61,30 @@ int moveLeft(int *line, size_t size)
 			}
 		}
 	}
-	return (1);
+}
+
+/**
+ * slide_line - Given an array of integers, we want to be able to slide
+ * & merge it to the left or to the right. Identical numbers, if they are
+ * contiguous or separated by zeros, should be merged
+ * Goal: The goal of this task is to reproduce the 2048 game(NSFW !!) mechanics
+ * on a single horizontal line.
+ * @line: points to an array of integers
+ * @size: Number of elements in array
+ * @direction: can be either SLIDE_LEFT or SLIDE_RIGHT
+ * Return: 1 upon success, or 0 upon failure
+ */
+int slide_line(int *line, size_t size, int direction)
+{
+	if (direction == SLIDE_LEFT)
+	{
+		move_left(line, size);
+		return (1);
+	}
+	if (direction == SLIDE_RIGHT)
+	{
+		move_right(line, size);
+		return(1);
+	}
+	return (0); /* Fail */
 }
