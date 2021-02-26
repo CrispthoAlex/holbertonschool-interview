589aa75a52b521837a2f74875a2f0b19ea1594a3
crispthoalex <alex877@gmail.com>
2021-02-26 04:44:03 +0000

    header file was updated

--- 0x0A-slide_line/slide_line.h
+++ 0x0A-slide_line/slide_line.h
@@ -1,11 +1,11 @@
 #ifndef SLIDE_H
 #define SLIDE_H
 
-#define SLIDE_LEFT
-#define SLIDE_RIGHT
+#include <stdlib.h>
 
+#define SLIDE_RIGHT 'R'
+#define SLIDE_LEFT 'L'
 
 int slide_line(int *line, size_t size, int direction);
-
 
 #endif /* SLIDE_H */
