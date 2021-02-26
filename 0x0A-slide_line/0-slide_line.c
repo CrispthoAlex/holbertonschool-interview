3f78b935eeb3ccb4c821f3d7d698768656de9f60
crispthoalex <alex877@gmail.com>
2021-02-26 04:36:07 +0000

    Slide_line and moveRight functions were added

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -7,12 +7,45 @@
  * Goal: The goal of this task is to reproduce the 2048 game(NSFW !!) mechanics
  * on a single horizontal line.
  * @line: points to an array of integers
- * @size: Number of elements in @array
+ * @size: Number of elements in array
  * @direction: can be either SLIDE_LEFT or SLIDE_RIGHT
  * Return: 1 upon success, or 0 upon failure
  */
 int slide_line(int *line, size_t size, int direction)
 {
+	if (direction == SLIDE_LEFT) {moveLeft(line, size);}
+	if (direction == SLIDE_RIGHt) {moveRight(line, size);}
 	return (0); /* Fail */
-	return (1); /* Succes*/
+}
+
+/**
+ * move_right - Slides and merges an array of integers to the right
+ * @line: Pointer to an array of integers
+ * @size: Number of elements in array
+ * Return: 1 is success
+ */
+int moveRight(int *line, size_t size)
+{
+	int i, j;
+
+	for (i = size - 1; i >= 0; i--)
+	{
+		for (j = i - 1; j >= 0; j--)
+		{
+			if (line[i] == 0 && line[j] > 0)
+			{
+				line[i] += line[j];
+				line[j] = 0;
+				i += 1;
+				break;
+			}
+			if (line[i] == 0 && line[j] > 0)
+			{
+				line[i] += line[j];
+				line[j] = 0;
+				break;
+			}
+		}
+	}
+	return (1);
 }
