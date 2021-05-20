1e41a9823100d3c1fd01c9433130286101bd4947
CrispthoAlex <alex877@gmail.com>
2021-05-20 11:36:16 -0500

    Script was fixed and updated

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,25 +1,36 @@
 #!/usr/bin/node
-// script that prints all characters of a Star Wars movie
+/*
+  Script that prints all characters of a Star Wars movie
+*/
 const movId = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + movId;
-const { isNumber } = require('lodash');
 const request = require('request');
 
-if (movId != undefined || isNumber(movId)) {
-    request(url, (error, response, body) => {
-      if (error) {
-        console.log(error);
-      } else {
-        const listChar = JSON.parse(body).characters;
+// https://javascript.info/async-await
+request(url, async (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const listChar = JSON.parse(body).characters;
+    // Test => console.log(listChar);
+    for (const urlCharacter of listChar) {
+      // Test url/api => console.log(`This is API character => ${urlCharacter}`);
+      console.log(await getNameChar(urlCharacter));
+    }
+  }
+});
 
-        listChar.forEach(character => {
-            request(character, (error, response, body) => {
-                if (error) { console.log(error); } else { console.log(JSON.parse(body).name); }
-              });
-        });
-      }
+/**
+  Function to catch the character name in order, using promise
+  https://javascript.info/promise-basics
+*/
+function getNameChar (apiCharacter) {
+  const promise = new Promise((resolve, reject) => {
+    request(apiCharacter, (error, response, body) => {
+      if (error) { reject(error); }
+      else { resolve(JSON.parse(body).name); }
     });
-    
-} else {
-    console.log("Usage:\t./0-starwars_characters.js [Movie ID]")
+  });
+  return promise;
 }
+
