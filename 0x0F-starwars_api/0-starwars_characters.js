ff642319a8f1100c0d8f4ac8c90a8540815be81d
CrispthoAlex <alex877@gmail.com>
2021-05-20 12:00:04 -0500

    Documentation was removed

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-/**
-  Script that prints all characters of a Star Wars movie
-*/
+
 const movId = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + movId;
 const request = require('request');
@@ -13,17 +11,15 @@ request(url, async (error, response, body) => {
   } else {
     const listChar = JSON.parse(body).characters;
     // Test => console.log(listChar);
-    for (const urlCharacter of listChar) {
+    for (const urlCharacter in listChar) {
       // Test url/api => console.log(`This is API character => ${urlCharacter}`);
       console.log(await getNameChar(urlCharacter));
     }
   }
 });
 
-/**
-  Function to catch the character name in order, using promise
-  https://javascript.info/promise-basics
-*/
+//  Function to catch the character name in order, using promise
+//  https://javascript.info/promise-basics
 function getNameChar (apiCharacter) {
   const promise = new Promise((resolve, reject) => {
     request(apiCharacter, (error, response, body) => {
