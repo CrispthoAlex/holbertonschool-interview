75cc84d1643cbd6be919822cbca95fd9633b41ae
CrispthoAlex <alex877@gmail.com>
2021-05-20 12:11:25 -0500

    Comments were removed and parameters were organized

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,32 +1,31 @@
 #!/usr/bin/node
 
 const movId = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/' + movId;
+
+const url = `https://swapi-api.hbtn.io/api/films/${movId}` ;
+
 const request = require('request');
 
-// https://javascript.info/async-await
 request(url, async (error, response, body) => {
+  // https://javascript.info/async-await
   if (error) {
     console.log(error);
   } else {
-    const listChar = JSON.parse(body).characters;
+    const listChar = JSON.parse(body).characters ;
     // Test => console.log(listChar);
-    for (const urlCharacter in listChar) {
+    for (const urlCharacter of listChar) {
       // Test url/api => console.log(`This is API character => ${urlCharacter}`);
-      console.log(await getNameChar(urlCharacter));
+      console.log(await getNameChar(urlCharacter)) ;
     }
   }
 });
 
-//  Function to catch the character name in order, using promise
-//  https://javascript.info/promise-basics
 function getNameChar (apiCharacter) {
   const promise = new Promise((resolve, reject) => {
     request(apiCharacter, (error, response, body) => {
-      if (error) { reject(error); }
-      else { resolve(JSON.parse(body).name); }
+      if (error) { reject(error) ; }
+      else { resolve(JSON.parse(body).name) ; }
     });
   });
   return promise;
 }
-
