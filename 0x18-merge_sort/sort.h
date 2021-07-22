929097aa3d1090e1d353419d61013e62f92feeed
CrispthoAlex <alex877@gmail.com>
2021-07-21 19:42:31 -0500

    Guarded was fixed

--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -1,5 +1,5 @@
-#ifndef _SHORT_H_
-#define _SHORT_H_
+#ifndef _SORT_H_
+#define _SORT_H_
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,4 +7,4 @@
 void merge_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 
-#endif /* _SHORT_H_ */
+#endif /* _SORT_H_ */
